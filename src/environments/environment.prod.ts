0bfc8c291ba64c43fe5bada298923915d25ef017
AlaAydi <contact.aydiala@gmail.com>
2026-06-21 01:28:41 +0100

    adding to neon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://trelloclonebackend-production.up.railway.app/api'
+  apiUrl: 'https://trelloclonebackend-production.up.railway.app/api',
+  aiApiUrl: 'https://trelloclonebackend-production.up.railway.app/api/ai'
 };
